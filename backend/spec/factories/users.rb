82deed17f7e077da26cce708934c27902f023dae
Otoriha <hardstyle9016@gmail.com>
2025-08-09 20:41:40 +0900

    create test file

---
+++ backend/spec/factories/users.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :user do
+    name { Faker::Name.name }
+    email { Faker::Internet.unique.email }
+    password { "password123" }
+    password_confirmation { "password123" }
+    
+    trait :confirmed do
+      confirmed_at { Time.current }
+    end
+  end
+end
