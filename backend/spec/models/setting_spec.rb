098f15a23d159e455a9c97ea8ee18d5fb359976c
Otoriha <hardstyle9016@gmail.com>
2025-10-21 15:40:14 +0900

    テスト追加

--- backend/spec/models/setting_spec.rb
+++ backend/spec/models/setting_spec.rb
@@ -16,6 +16,22 @@ RSpec.describe Setting, type: :model do
     it { is_expected.to validate_inclusion_of(:cooking_time).in_array([ 15, 30, 60, 999 ]) }
   end
 
+  describe "通知設定のバリデーション" do
+    describe "inventory_reminder_enabled" do
+      it { is_expected.to allow_value(true).for(:inventory_reminder_enabled) }
+      it { is_expected.to allow_value(false).for(:inventory_reminder_enabled) }
+    end
+
+    describe "inventory_reminder_time" do
+      it { is_expected.to validate_presence_of(:inventory_reminder_time) }
+
+      it "有効な時刻を受け入れる" do
+        setting = build(:setting, inventory_reminder_time: Time.zone.parse('09:00'))
+        expect(setting).to be_valid
+      end
+    end
+  end
+
   describe "デフォルト値" do
     it "正しいデフォルト値を持つ" do
       setting = Setting.new
@@ -23,6 +39,8 @@ RSpec.describe Setting, type: :model do
       expect(setting.recipe_difficulty).to eq("medium")
       expect(setting.cooking_time).to eq(30)
       expect(setting.shopping_frequency).to eq("2-3日に1回")
+      expect(setting.inventory_reminder_enabled).to eq(false)
+      expect(setting.inventory_reminder_time.strftime('%H:%M')).to eq('09:00')
     end
   end
 end
